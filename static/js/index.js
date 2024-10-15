55ecfce3aad2ded81af0f907a91c1072741cde70
4d-worldmodel <183551913+4d-worldmodel@users.noreply.github.com>
2024-10-14 23:28:09 -0400

    update

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,7 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
 // ====== INTERPOLATION for stacked images ======
-var INTERP_BASE = "./static/interpolation/stacked";
+var INTERP_BASE = "./static/interpolation/sample1";
 var NUM_INTERP_FRAMES = 15;
 
 var interp_images = [];
@@ -22,7 +22,7 @@ function setInterpolationImage(i) {
 
 // ====== MODEL VIEWER ======
 const NUM_MODELS = 15;
-const MODEL_BASE = "https://4d-worldmodel.github.io/models/";
+const MODEL_BASE = "https://4d-worldmodel.github.io/models/sample1";
 const modelFiles = [];
 
 function preloadModelFiles() {
@@ -36,6 +36,45 @@ function preloadModelFiles() {
 function setModel(index) {
   const modelViewer = document.getElementById('model-viewer');
   modelViewer.src = modelFiles[index];
+}
+
+
+// ====== INTERPOLATION for stacked images ======
+var INTERP_BASE2 = "./static/interpolation/sample2";
+var NUM_INTERP_FRAMES2 = 34;
+
+var interp_images2 = [];
+function preloadInterpolationImages2() {
+  for (var i = 0; i < NUM_INTERP_FRAMES2; i++) {
+    var path = INTERP_BASE2 + '/' + String(i).padStart(6, '0') + '.png';
+    interp_images2[i] = new Image();
+    interp_images2[i].src = path;
+  }
+}
+
+function setInterpolationImage2(i) {
+  var image = interp_images2[i];
+  image.ondragstart = function () { return false; };
+  image.oncontextmenu = function () { return false; };
+  $('#interpolation-image-wrapper2').empty().append(image);
+}
+
+// ====== MODEL VIEWER ======
+const NUM_MODELS2 = 34;
+const MODEL_BASE2 = "https://4d-worldmodel.github.io/models/sample2";
+const modelFiles2 = [];
+
+function preloadModelFiles2() {
+  for (let i = 0; i < NUM_MODELS2; i++) {
+    const fileName = String(i).padStart(6, '0') + '.glb';
+    const filePath = `${MODEL_BASE2}/${fileName}`;
+    modelFiles2.push(filePath);
+  }
+}
+
+function setModel2(index) {
+  const modelViewer = document.getElementById('model-viewer2');
+  modelViewer.src = modelFiles2[index];
 }
 
 
@@ -79,13 +118,11 @@ $(document).ready(function () {
   }
 
   preloadInterpolationImages();
-
   $('#model-slider').on('input', function (event) {
     setInterpolationImage(this.value);
   });
   setInterpolationImage(14);
   $('#model-slider').prop('max', NUM_INTERP_FRAMES - 1);
-
   preloadModelFiles();
   $('#model-slider').on('input', function () {
     const index = parseInt(this.value);
@@ -93,6 +130,20 @@ $(document).ready(function () {
   });
   setModel(14);
   $('#model-slider').prop('max', NUM_MODELS - 1);
+
+  preloadInterpolationImages2();
+  $('#model-slider2').on('input', function (event) {
+    setInterpolationImage2(this.value);
+  });
+  setInterpolationImage2(33);
+  $('#model-slider2').prop('max', NUM_INTERP_FRAMES2 - 1);
+  preloadModelFiles2();
+  $('#model-slider2').on('input', function () {
+    const index = parseInt(this.value);
+    setModel2(index);
+  });
+  setModel2(33);
+  $('#model-slider2').prop('max', NUM_MODELS2 - 1);
 
   bulmaSlider.attach();
 
